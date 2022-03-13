54ec904104e792ffdcfe2a6b900e994ce73a86d3
Chen Ching Yen <92667133+ChenChingYen@users.noreply.github.com>
2022-03-14 00:58:31 +0800

    Update test_page_js.js

--- test_page_js.js
+++ test_page_js.js
@@ -1,6 +1,8 @@
 const closeInfoBtn = document.querySelector(".close-info-btn");
 const info = document.querySelector(".info-wrapper");
+const qrwrapper = document.querySelector(".qr-wrapper");
 
 closeInfoBtn.addEventListener("click", function(){
     info.classList.add("close-info");
+    qrwrapper.classList.remove("close-info");
 });
