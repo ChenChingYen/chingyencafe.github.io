30351b0547552575a86b68d58137bfd7e44c6b74
Chen Ching Yen <92667133+ChenChingYen@users.noreply.github.com>
2022-03-14 01:13:51 +0800

    Update test_page_js.js

--- test_page_js.js
+++ test_page_js.js
@@ -5,4 +5,5 @@ const qrscanner = document.querySelector(".qr-wrapper");
 closeInfoBtn.addEventListener("click", function(){
     info.classList.add("close-info");
     qrscanner.classList.remove("close-info");
+    alert('success');
 });
